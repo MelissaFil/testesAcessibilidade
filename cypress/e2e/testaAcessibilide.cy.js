9ebb93056fa829dc79d68fea0424a34791716d50
unknown <programadorimd@hotmail.com>
2023-10-30 23:56:44 -0300

    contando violações por nível de impacto

--- cypress/e2e/testaAcessibilide.cy.js
+++ cypress/e2e/testaAcessibilide.cy.js
@@ -1,34 +1,3 @@
-var cont=0;
-function terminalLog(violations) {
-  cy.task(
-    'log',
-    `${violations.length} violaç${
-      violations.length === 1 ? 'ão de acessibilidade' : 'ões de acessibilidade'
-    } ${violations.length === 1 ? 'foi detectada' : 'foram detectadas'} `
-  )
-  // pluck specific keys to keep the table readable
-  const violationData = violations.map(
-    ({ id, impact, description, nodes }) => ({
-      id,
-      impact,
-      description,
-      nodes: nodes.length
-    })
-  )
-
-  cy.task('table', violationData)
-  contViolation(violations)
-}
-
-
-function contViolation (violation){
-  cont+=violation.length;
- 
-  cy.task(
-    'log',
-    `Um total de ${cont} violações encontradas no sistema `
-  )
-}
 
 describe('testeAcessibilidade ', () => {
   beforeEach(() => {  
@@ -36,20 +5,20 @@ describe('testeAcessibilidade ', () => {
  })
 
   it('verifica violações em início', () => {
-    cy.checkLink('', terminalLog)
+    cy.checkLink('')
    
   })
 
   it('verifica violação em cidadão',() => {
-    cy.checkLink('Cidadão',terminalLog)
+    cy.checkLink('Cidadão')
     
   })
   it('verifica violação em turista',() => {
-    cy.checkLink('Turista',terminalLog)
+    cy.checkLink('Turista')
     
   })
   it('verifica violação em empresas',() => {
-    cy.checkLink('Empresas',terminalLog)
+    cy.checkLink('Empresas')
     
   })
 

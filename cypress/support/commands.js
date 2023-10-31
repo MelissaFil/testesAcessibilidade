3e1c4be5484fb96ed8be8f8a0741696947f611f5
unknown <programadorimd@hotmail.com>
2023-10-31 00:23:25 -0300

    trocando ifelse por switch

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,60 +1,26 @@
-var cont=0;
-var contmoderate=0;
-var contserious=0;
-var contcritical=0;
-function terminalLog(violations) {
-  cy.task(
-    'log',
-    `${violations.length} violaç${
-      violations.length === 1 ? 'ão de acessibilidade' : 'ões de acessibilidade'
-    } ${violations.length === 1 ? 'foi detectada' : 'foram detectadas'} `
-  )
-  // pluck specific keys to keep the table readable
-  const violationData = violations.map(
-    ({ id, impact, description, nodes }) => ({
-      id,
-      impact,
-      description,
-      nodes: nodes.length
-    })
-  )
-
-  cy.task('table', violationData)
-  contViolation(violations)
-}
-
-
+var cont=0, contmoderate=0, contserious=0, contcritical=0;
 function contViolation (violation){
   cont+=violation.length;
 
   violation.map(function(v){
-    if(v.impact == 'moderate'){
-        contmoderate++
-      }else
-    if(v.impact == 'serious'){
-        contserious++
-    }else
-    if(v.impact == "critical"){
-        contcritical++
-      }
+    switch(v.impact) {
+        case 'moderate':
+            contmoderate++
+            break;
+        case 'serious':
+            contserious++
+            break;
+        case "critical":
+            contcritical++
+            break;      }
   })
 
-  cy.task(
-    'log',
-    `Um total de ${cont} violações encontradas no sistema `
+  cy.task('log', `Um total de ${cont} violações encontradas no sistema onde:`+
+    `\n${contserious} violações são Sérias` + 
+    `\n ${contmoderate} violações são moderadas` +
+    `\n ${contcritical} violações são Críticas `
   )
-  cy.task(
-    'log',
-    `Um total de ${contmoderate} violações moderadas encontradas no sistema `
-  )
-  cy.task(
-    'log',
-    `Um total de ${contserious} violações Sérias encontradas no sistema `
-  )
-  cy.task(
-    'log',
-    `Um total de ${contcritical} violações Críticas encontradas no sistema `
-  )
+
  
 }
 
@@ -63,5 +29,5 @@ Cypress.Commands.add('checkLink', (buttonContains) => {
         cy.contains('a', buttonContains).click()
     }
     cy.injectAxe() 
-    cy.checkA11y(null, null, terminalLog, true)  
+    cy.checkA11y(null, null, contViolation, true)  
 })

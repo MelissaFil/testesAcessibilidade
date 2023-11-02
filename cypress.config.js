0d76bfc3e443d3da8bb7e531a3cb2559fc5ca8be
unknown <programadorimd@hotmail.com>
2023-11-02 13:22:15 -0300

    fazendo teste em fullhd para evitar menu hamburguer

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("cypress");
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {

95d95bb0664e4373a79f379474f375d9f497afdb
unknown <programadorimd@hotmail.com>
2023-11-04 02:34:49 -0300

    retirando task que não estava sendo usada

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {
-        log(message) {
-          console.log(message)
-
-          return null
-        },
         table(message) {
           console.table(message)
     
